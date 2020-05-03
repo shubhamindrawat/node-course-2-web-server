197f843cb342c3a4259b81088d4478a93126fb2b
Shubham Jain <shubhamj@aosmithindia.com>
2020-05-03 13:11:29 +0530

    Some changes

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use((req, res, next) => {
-//     res.render('maintenance');
-// });
+app.use((req, res, next) => {
+    res.render('maintenance');
+});
 
 app.use(express.static(__dirname + '/public'));
 
